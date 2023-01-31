5318d791c6014f5336b74f8a14179be967a68c17
alaodev <alaodev@gmail.com>
2023-01-31 19:32:19 -0300

    refactor: componentizes app bar

---
+++ components/app-bar..vue
@@ -0,0 +1,6 @@
+<template>
+  <v-app-bar>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-toolbar-title>Page Title</v-toolbar-title>
+  </v-app-bar>
+</template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -5,12 +5,7 @@ const drawer = ref(true);
 <template>
   <v-app>
     <navigation-drawer v-model="drawer" />
-    <v-app-bar>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
-      <v-toolbar-title>Page Title</v-toolbar-title>
-    </v-app-bar>
-
+    <app-bar />
     <v-main>
       <v-container fluid>
         <slot />
