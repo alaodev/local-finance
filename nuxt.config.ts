3854d428a9e099f539efb3343b9fa8c77d9fe105
alaodev <alaodev@gmail.com>
2023-01-31 19:35:08 -0300

    chore: configure pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,6 +7,7 @@ export default defineNuxtConfig({
     "@mdi/font/css/materialdesignicons.min.css",
     "vuetify/lib/styles/main.sass",
   ],
+  modules: [["@pinia/nuxt", { autoImports: ["defineStore", "storeToRefs"] }]],
   vite: {
     define: {
       "process.env.DEBUG": false,
