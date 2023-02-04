ea6daa12bccc5b231acc2e2daee8633c67d54435
alaodev <alaodev@gmail.com>
2023-02-04 20:06:42 -0300

    feat: implement floating action button

---
+++ components/fab.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-btn class="fab" color="primary" />
+</template>
+
+<style lang="scss" scoped>
+.fab {
+  bottom: 1rem;
+  position: fixed;
+  right: 1rem;
+}
+</style>
--- pages/wallets/index.vue
+++ pages/wallets/index.vue
@@ -8,16 +8,9 @@ const { wallets } = storeToRefs(walletsStore);
 
 <template>
   <v-row>
-    <v-col class="d-flex" cols="12">
-      <v-spacer />
-      <v-btn color="primary" to="/wallets/create" variant="flat">{{
-        $t("pages.wallets.index.new-wallet")
-      }}</v-btn>
-    </v-col>
-  </v-row>
-  <v-row>
     <v-col v-for="wallet of wallets" cols="12" sm="6" md="4">
       <wallet-card :wallet="wallet" />
     </v-col>
   </v-row>
+  <fab icon="mdi-plus" to="/wallets/create" />
 </template>
