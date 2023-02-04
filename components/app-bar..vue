22af13e53b6a83af949356dd4f97b9c508a845c9
alaodev <alaodev@gmail.com>
2023-02-04 00:49:19 -0300

    chore: remove breadcrumbs

--- components/app-bar..vue
+++ components/app-bar..vue
@@ -1,35 +1,5 @@
-<script lang="ts" setup>
-import { capitalize } from "~~/utils/formatters";
-
-const route = useRoute();
-
-const breadcrumbItems = computed(() => {
-  const splittedFullPath = route.fullPath.split("/").slice(1);
-  const items = [];
-  let currentPath = "/";
-
-  for (let path of splittedFullPath) {
-    currentPath += `${path}/`;
-    items.push({
-      title: capitalize(path),
-      to: currentPath,
-    });
-  }
-
-  return items;
-});
-
-route.fullPath
-  .split("/")
-  .slice(1)
-  .map((item) => ({ title: capitalize(item) }));
-
-console.log(breadcrumbItems);
-</script>
-
 <template>
   <v-app-bar>
-    <v-breadcrumbs :items="breadcrumbItems"> </v-breadcrumbs>
     <template v-slot:append>
       <theme-switcher />
       <lang-switcher />
