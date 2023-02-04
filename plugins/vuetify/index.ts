af957c7a06db340c5a1261bbe89df537784ae318
alaodev <alaodev@gmail.com>
2023-02-04 13:16:39 -0300

    style: add them file

--- plugins/vuetify/index.ts
+++ plugins/vuetify/index.ts
@@ -2,27 +2,14 @@ import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 import defaults from "~~/plugins/vuetify/defaults";
+import theme from "~~/plugins/vuetify/theme";
 
 export default defineNuxtPlugin((nuxtApp) => {
   const vuetify = createVuetify({
     components,
     defaults,
     directives,
-    theme: {
-      themes: {
-        light: {
-          colors: {
-            primary: "#009ee3",
-          },
-        },
-        dark: {
-          dark: true,
-          colors: {
-            primary: "#009ee3",
-          },
-        },
-      },
-    },
+    theme,
   });
 
   nuxtApp.vueApp.use(vuetify);
---
+++ plugins/vuetify/theme.ts
@@ -0,0 +1,16 @@
+export default {
+  themes: {
+    light: {
+      colors: {
+        primary: "#009ee3",
+        background: "#ededed",
+      },
+    },
+    dark: {
+      dark: true,
+      colors: {
+        primary: "#008ad6",
+      },
+    },
+  },
+};
