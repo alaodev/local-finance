1c41842fcc38332167b86eb1d29e04159ff79591
alaodev <alaodev@gmail.com>
2023-02-04 00:57:13 -0300

    chore: implement return button layout

---
+++ layouts/return-button-layout.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+type Props = {
+  returnTo?: string;
+};
+
+const props = withDefaults(defineProps<Props>(), {
+  returnTo: "/",
+});
+</script>
+
+<template>
+  <v-row>
+    <v-col>
+      <v-btn :to="props.returnTo" icon="mdi-arrow-left" />
+    </v-col>
+  </v-row>
+  <slot />
+</template>
--- pages/wallets/create/index.vue
+++ pages/wallets/create/index.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import ReturnButtonLayout from "~~/layouts/return-button-layout.vue";
 import WalletForm from "~~/pages/wallets/forms/create-form.vue";
 
 const walletsStore = useWallets();
@@ -18,27 +19,24 @@ function handleCreateWallet() {
 </script>
 
 <template>
-  <v-row>
-    <v-col>
-      <v-btn icon="mdi-arrow-left" to="/wallets/" />
-    </v-col>
-  </v-row>
-  <v-row>
-    <v-col>
-      <v-card class="mx-auto pa-6" max-width="700">
-        <v-card-title class="mb-5">
-          {{ $t("pages.wallets.create.card-title") }}
-        </v-card-title>
-        <v-card-text>
-          <wallet-form ref="form" />
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn @click="handleCreateWallet">{{
-            $t("pages.wallets.create.create-btn")
-          }}</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+  <return-button-layout return-to="/wallets/">
+    <v-row>
+      <v-col>
+        <v-card class="mx-auto pa-6" max-width="700">
+          <v-card-title class="mb-5">
+            {{ $t("pages.wallets.create.card-title") }}
+          </v-card-title>
+          <v-card-text>
+            <wallet-form ref="form" />
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer />
+            <v-btn @click="handleCreateWallet">{{
+              $t("pages.wallets.create.create-btn")
+            }}</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </return-button-layout>
 </template>
