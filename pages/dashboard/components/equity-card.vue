37ec9d7a466f8f9e0996b22f3269f5b99494edb1
alaodev <alaodev@gmail.com>
2023-02-26 22:40:40 -0300

    feat: implement equity dynamic currency value and internationalization

--- pages/dashboard/components/equity-card.vue
+++ pages/dashboard/components/equity-card.vue
@@ -1,20 +1,26 @@
 <script lang="ts" setup>
+import { currencyNumber } from "~~/utils/formatters";
+
 type Props = {
   equityValue: number;
 };
 
 const props = defineProps<Props>();
+
+const currencyStore = useCurrency();
+
+const { currency } = storeToRefs(currencyStore);
 </script>
 
 <template>
   <v-card
+    :title="$t('pages.dashboard.total-equity-card.title')"
+    :subtitle="$t('pages.dashboard.total-equity-card.subtitle')"
     class="pa-6"
-    title="Patrimônio total"
-    subtitle="Somatório de todas as categorias"
   >
     <v-sheet class="pa-6">
-      <span class="text-h3 text-primary">
-        {{ Number(props.equityValue).toFixed(2) }}
+      <span class="text-h4 text-primary">
+        {{ currencyNumber(props.equityValue, currency) }}
       </span>
     </v-sheet>
   </v-card>
